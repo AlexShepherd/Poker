94c820c96dddce6006e5abd0e120312612e6ac61
AlexShepherd28 <42952769+AlexShepherd28@users.noreply.github.com>
2019-09-27 22:04:47 -0400

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -1,5 +1,40 @@
 public class Card
 {
-enum Rank;
-enum Suit;
+    private Rank rank;
+    private Suit suit;
+
+    //no-arg constructor
+    public Card ()
+    {
+        rank = Rank.ACE;
+        suit = Suit.HEARTS;
+    }
+
+    public Card (Rank rank, Suit suit)
+    {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    //copy constructor
+    public Card(Card existingCard)
+    {
+        this.rank = existingCard.rank;
+        this.suit = existingCard.suit;
+    }
+
+    //parameterized constructor
+    public Card(int n)
+    {
+        rank = Rank.values()[n % 13];
+        suit = Suit.values()[n % 4];
+    }
+
+    @Override
+    public String toString ( )
+    {
+        String resultOut = "The " + rank + " of " + suit;
+
+        return resultOut;
+    } // end toString
 }
